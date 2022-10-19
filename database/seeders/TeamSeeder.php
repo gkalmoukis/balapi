a1f2b5513fb6e7e4a39cacf78e95622188792816
gkalmoukis <giorgoskalmoukis@gmail.com>
2022-10-19 23:15:22 +0300

    Add teams table with seeder

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Team;
 
 class TeamSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class TeamSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Team::factory(10)->create();
     }
 }
