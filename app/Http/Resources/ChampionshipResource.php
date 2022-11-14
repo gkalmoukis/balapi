ba033c41d4585d053ce799744cfc3876a6ac8ce6
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2022-11-14 13:11:18 +0200

    Add championship resources

--- app/Http/Resources/ChampionshipResource.php
+++ app/Http/Resources/ChampionshipResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\GameCollection;
 
 class ChampionshipResource extends JsonResource
 {
@@ -14,6 +15,12 @@ class ChampionshipResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "games" => new GameCollection($this->whenLoaded('games')),
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at
+        ];
     }
 }
